77e874322e2e5a05831a0c5c36551f09276a4405
codith <marcus.carr@gmail.com>
2014-07-26 18:07:49 -0400

    Added changes to files.

--- README.md
+++ README.md
@@ -2,3 +2,19 @@ gettingdata
 ===========
 
 Git repository for Getting and Cleaning Data Coursera project
+
+The data are as received from UCI and placed in the directory ../data/
+
+The script reads the following from ../data/UCI_HAR_Dataset:
+* activity_labels.txt -- contains the conversion from numerical to
+  descriptive activities
+* features.txt -- contains the column labels for the data sets
+* train/X_train.txt test/X_test.txt -- contains the readouts for the
+  different variables
+* train/y_train.txt test/y_test.txt -- contains the activity labels for
+  each of the observations in the X files
+
+Outputs are two CSV files in the UCI_HAR_Dataset directory:
+data_set.csv -- contains all data pertaining to mean and stdev
+agg_data_set.csv -- contains means for each variable by subject and
+activity
--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,8 @@
+col_names <- read.table("data/UCI_HAR_Dataset/features.txt")
+activity_df <- read.table("data/UCI_HAR_Dataset/activity_labels.txt"
+                          col.names = c("activityCode", "activityName"))
+
+train_data <- read.table("data/UCI_HAR_Dataset/train/X_train.txt", col.names = col_names)
 train_data_filt <- train_data[,mean_std_cols]
 train_subject <- read.table("data/UCI_HAR_Dataset/train/subject_train.txt",  
                             col.name = "subject")
@@ -13,3 +18,13 @@ test_subject <- read.table("data/UCI_HAR_Dataset/test//subject_test.txt",
 test_activity <- read.table("data/UCI_HAR_Dataset//test//y_test.txt", 
                             col.name = "activity")
 test_set <- cbind(test_data_filt, test_subject, test_activity)
+
+# Combine the two data sets
+data_set <- rbind(train_set, test_set)
+agg_test_data_filt <- aggregate(test_data_filt, 
+                                by = as.list(c(test_subject, test_activity)), 
+                                FUN = "mean")
+
+# Write output
+write.csv(data_set, file = "data/UCI_HAR_Dataset/data_set.csv")
+write.csv(agg_data_set, file = "data/UCI_HAR_Dataset/agg_data_set.csv")
